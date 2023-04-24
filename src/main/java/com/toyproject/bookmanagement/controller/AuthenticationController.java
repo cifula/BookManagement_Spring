91047905134e28d1803c0850aafa315fb9e92843
cifula <lidel95@gmail.com>
2023-04-24 14:04:16 +0900

    no message

--- src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
+++ src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
@@ -43,6 +43,11 @@ public class AuthenticationController {
 	public ResponseEntity<?> authenticated(String accessToken) {
 		return ResponseEntity.ok().body(authenticationService.authenticated(accessToken));
 	}
+	
+	@GetMapping("/principal")
+	public ResponseEntity<?> principal(String accessToken) {
+		return ResponseEntity.ok().body(authenticationService.getPrincipal(accessToken));
+	}
 }
 
 
